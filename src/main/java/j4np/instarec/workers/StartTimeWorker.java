2c21404a986aa39b2b869685076b83e57eb29ef0
asligonulacar <67739979+asligonulacar@users.noreply.github.com>
2025-02-13 00:19:41 +0000

    Update StartTimeWorker.java
    require status>0 to pick the highest momentum electron.

--- src/main/java/j4np/instarec/workers/StartTimeWorker.java
+++ src/main/java/j4np/instarec/workers/StartTimeWorker.java
@@ -1,4 +1,5 @@
 package j4np.instarec.workers;
+
 import j4np.data.base.DataActor;
 import j4np.data.base.DataEvent;
 import j4np.data.base.DataSource;
@@ -9,6 +10,9 @@ import j4np.hipo5.data.Leaf;
 import j4np.hipo5.data.Schema;
 import j4np.hipo5.data.Schema.SchemaBuilder;
 import j4np.instarec.utils.NeuralModel;
+import twig.data.H1F;
+import twig.data.TDirectory;
+import java.util.ArrayList;
 
 /**
  * StartTimeWorker class that calculates and saves a histogram of start times.
@@ -20,34 +24,33 @@ public class StartTimeWorker extends DataWorker {
         return true;
     }
 
+    private ArrayList<Double> startTimes = new ArrayList<>();
+    private static final String OUTPUT_TWIG_FILE = "start_time_distribution.twig"; // Output file
 
     public void CalculateStartTime(Leaf partout, Leaf Startout, short row) {
-        // Speed of light in cm/ns
-        double c = 29.9792;
-        // Get time of flight and path from partout bank written by ElPID worker:
-        double t_flight = partout.getFloat(28, row);
-        double path = partout.getFloat(27, row);
-        // Calculate start time for the event assuming beta=1:
+        double c = 29.9792; // Speed of light in cm/ns
+        double t_flight = partout.getDouble(28, row);
+        double path = partout.getDouble(27, row);
         double t_0 = t_flight - (path / c);
         System.out.println("Start Time: " + t_0);
 
         Startout.putDouble(0, 0, t_0);
+        startTimes.add(t_0);
     }
 
-    public short CalculateHighestMomentumElectron(Leaf partout, Leaf Startout) {
-        // Finds the highest momentum electron in any given event.
-        float maxP = 0;
+    public short CalculateHighestMomentumElectron(Leaf partout, Leaf Startout, Leaf trackbank) {
+        double maxP = 0;
         short maxPRow = -1;
 
         for (short i = 0; i < partout.getRows(); i++) {
             if (partout.getInt(1, i) == 11 && partout.getInt(4, i) == -1) {
-                float px = partout.getFloat(5, i);
-                float py = partout.getFloat(6, i);
-                float pz = partout.getFloat(7, i);
-                //Calculate absolute value of 3-momentum:
-                float rho = (float) Math.sqrt(px * px + py * py + pz * pz);
-                //Update electron with max momentum dynamically:
-                if (rho > maxP) {
+                double px = partout.getDouble(6, i);
+                double py = partout.getDouble(7, i);
+                double pz = partout.getDouble(8, i);
+                double rho = (double) Math.sqrt(px * px + py * py + pz * pz);
+                short pindex = partout.getShort(0, i);
+                double status = trackbank.getInt(0, pindex);
+                if (rho > maxP && status>0 ) {
                     maxP = rho;
                     maxPRow = i;
                 }
@@ -65,22 +68,43 @@ public class StartTimeWorker extends DataWorker {
     @Override
     public void execute(DataEvent event) {
         Leaf partout = new Leaf(32200, 1, "sifssf3f3f6f9f3ffff", 4096);
+        ((Event) event).read(partout);
 
         Leaf Startout = new Leaf(32200, 4, "d", 4096);
         Startout.setRows(1);
 
-        short maxPRow = CalculateHighestMomentumElectron(partout, Startout);
+        Leaf trackbank = new Leaf(32000,1,"i",4096);
+        ((Event) event).read(trackbank);
+
+        short maxPRow = CalculateHighestMomentumElectron(partout, Startout, trackbank);
         if (maxPRow != -1) {
-            // If there is at least one electron in the event:
             CalculateStartTime(partout, Startout, maxPRow);
-            System.out.println("StartTimes array size: " + startTimes.size());
+            //System.out.println("StartTimes array size: " + startTimes.size());
         } else {
-            // If no electron, put the start time as -999.
             Startout.putDouble(0, 0, -999);
         }
-        // Write the event.
+
         ((Event) event).write(Startout);
+
+       //System.out.println("🔹 DEBUG: finish() method called.");
+        //System.out.println("Total startTimes size: " + startTimes.size());
+
+        //if (startTimes.isEmpty()) {
+            //System.out.println("No start times recorded, skipping histogram save.");
+            //return;
+        //}
+
+        // Create histogram
+        //H1F hist = new H1F("Start Time Distribution", 1000, 0.0, 10000.0);
+        //for (double t : startTimes) {
+            //hist.fill(t);
+        //}
+        //String filePath = System.getProperty("user.dir") + "/" + OUTPUT_TWIG_FILE;
+        //System.out.println("🔹 DEBUG: Saving histogram to " + filePath);
+        // Save histogram using `TDirectory.export()`
+        //TDirectory.export(OUTPUT_TWIG_FILE, "/plots/StartTime", hist);
+        //System.out.println("Histogram saved as " + OUTPUT_TWIG_FILE);
     }
-    }
+}
 
 
